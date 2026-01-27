e7cd81b4a0ff69f34f2636b33e77b63c7e8b4f31
Teratto <Crunchapotato@gmail.com>
2026-01-27 06:01:04 -0800

    ughhhh bailout doesn't work

--- Features/TeraBailoutManager.cs
+++ Features/TeraBailoutManager.cs
@@ -9,6 +9,8 @@ using Nickel;
 using TeraTaxMod.Features;
 using static TeraTaxMod.External.IKokoroApi.IV2.IStatusLogicApi.IHook;
 using Microsoft.Extensions.Logging;
+using TeraTaxMod.Artifacts;
+using System.Net.NetworkInformation;
 
 namespace TeraTaxMod.Features;
 
@@ -17,31 +19,47 @@ public class TeraBailoutManager : IKokoroApi.IV2.IStatusLogicApi.IHook
 {
 
 
+    //isGood = true 
+    // ----- pos int, do nothing. neg int, negate
+    //isGood = false
+    // ----- pos int, negate, neg int, do nothing
+    //isGood is set to a value
+    public bool CanHandleImmediateStatusTrigger(ICanHandleImmediateStatusTriggerArgs args)
+    {
+        return true;
+    }
+
+    //[HarmonyPrefix]
+    //[HarmonyPatch(typeof(AStatus), "Begin")]
+    public static void AStatus_Begin_Prefix(AStatus __instance, State s, Combat c)
+    {
+        //if ((__instance.status.isgood && __instance.statusAmount > 0) || (status.isGood && __instance.statusAmount < 0)) { 
+            
+           // __instance.statusAmount = 0;
+            
+                
+        
+        
+    }
+    public void HandleImmediateStatusTrigger(IHandleImmediateStatusTriggerArgs args) 
+        {
+          //  if (args.Status != ModEntry.Instance.TeraBailoutStatus.Status) {  return; }
+           // bool isPlayerShip = args.Ship.isPlayerShip;
+           // bool isItGood = !DB.statuses[args.Status].isGood && args.Status != ModEntry.Instance.TeraBailoutStatus.Status;
+           // if ((!args.status.isGood && amount > 0) || (status.isGood && amount < 0)) { amount = 0; }
+        }
 
 
+
+
+    //NOTE: INCLUDE CODE THAT MAKES THE ISMISSING DIALOGUE ONLY RUN IF BAILOUT IS NOT TRIGGERED. 
+    //Detect every status a ship gets ever.
+    //if that status isgood and -int, set value to 0 (before ship gains status) and -1 bailout
+    // do the opposite for when the status is not good
+        
     public bool HandleStatusTurnAutoStep(IHandleStatusTurnAutoStepArgs args)
     {
-        if (args.Status != ModEntry.Instance.TeraPersistenceStatus.Status)
-            return false;
         
-        if (args.Timing != IKokoroApi.IV2.IStatusLogicApi.StatusTurnTriggerTiming.TurnStart)
-            return false;
-        
-        int persistenceAmount = args.Amount;
-        bool isPlayerShip = args.Ship.isPlayerShip;
-        
-  
-        args.Combat.QueueImmediate(new AStatus()
-            {
-                status = ModEntry.Instance.TeraTaxationStatus.Status,
-                statusAmount = persistenceAmount,
-                targetPlayer = isPlayerShip
-
-            });
-        
-
-
-
         return false;
     }
 }
--- Features/TeraTaxationManager.cs
+++ Features/TeraTaxationManager.cs
@@ -8,21 +8,23 @@ using Nanoray.PluginManager;
 using Nickel;
 using static TeraTaxMod.External.IKokoroApi.IV2.IStatusLogicApi.IHook;
 using TeraTaxMod.Artifacts;
+using JetBrains.Annotations;
 
 namespace TeraTaxMod.Features;
-
-
+ 
 public class TeraTaxationManager : IKokoroApi.IV2.IStatusLogicApi.IHook
 {
     public void OnStatusTurnTrigger(IOnStatusTurnTriggerArgs args)
     {
-        bool inflationGet = s.artifacts.Find(a => a.GetType() == typeof(Inflation)) != null;
+        
+        bool inflationGet = args.State.EnumerateAllArtifacts().Find(a => a.GetType() == typeof(Inflation)) != null;
+        int taxationDamageThreshold = inflationGet ? 2 : 3;
+
 
         if (args.Status != ModEntry.Instance.TeraTaxationStatus.Status)
             return;
         if (args.Timing != IKokoroApi.IV2.IStatusLogicApi.StatusTurnTriggerTiming.TurnEnd)
             return;
-        int taxationDamageThreshold = inflationGet == true ? 2 : 3;
         
         bool isPlayerShip = args.Ship.isPlayerShip;
         if (args.Ship.Get(ModEntry.Instance.TeraTaxationStatus.Status) >= taxationDamageThreshold) 
